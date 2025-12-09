ebbb16e9a64b5523b0e7855e05adc03b679f8f28
高学宇 <75412701@qq.com>
2025-12-09 17:45:04 +0800

    后台ping接口提交

---
+++ service/router/system/ping.go
@@ -0,0 +1,14 @@
+package system
+
+import (
+	"sun-panel/api/api_v1"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitPingRouter(router *gin.RouterGroup) {
+	ping := api_v1.ApiGroupApp.ApiSystem.Ping
+	{
+		router.GET("system/ping", ping.Get)
+	}
+}
